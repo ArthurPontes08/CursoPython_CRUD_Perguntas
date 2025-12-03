35ab6e1fd93d38e029f649be619d39e4c1b5f7e5
devArthur <arthurpontesgp@gmail.com>
2025-12-03 19:28:52 -0300

    Criar janela principal da interface

--- interface_crud.py
+++ interface_crud.py
@@ -2,4 +2,8 @@ import tkinter as tk
 from tkinter import ttk, messagebox 
 import logica 
 
-
+def iniciar_interface(): 
+ janela = tk.Tk() 
+ janela.title("CRUD de Perguntas") 
+ janela.geometry("850x500") 
+ perguntas = logica.carregar_dados()
