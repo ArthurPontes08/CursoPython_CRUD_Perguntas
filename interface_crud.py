3a8eed41ef184ffe220a44f735c44801cefcfb77
devArthur <arthurpontesgp@gmail.com>
2025-12-09 19:54:14 -0300

    Alteração do Botão Adicionar

--- interface_crud.py
+++ interface_crud.py
@@ -67,7 +67,7 @@ scrollbar.pack(side="right", fill="y")
 tree.pack(fill="both", expand=True)
 
 
-btn_adicionar = ttk.Button(frame_top, text="Adicionar")
+btn_adicionar = ttk.Button(frame_top, text="Adicionar Pergunta")
 btn_adicionar.grid(row=3, column=0, padx=5, pady=10)
 
 btn_atualizar = ttk.Button(frame_top, text="Atualizar")
@@ -78,6 +78,11 @@ btn_excluir.grid(row=3, column=2, padx=5, pady=10)
 
 btn_limpar = ttk.Button(frame_top, text="Limpar")
 btn_limpar.grid(row=3, column=3, padx=5, pady=10)
+
+
+
+
+
 
 
 
