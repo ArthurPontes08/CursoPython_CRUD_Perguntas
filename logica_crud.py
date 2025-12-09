69977d5c60ab3f6eaf96f1b6330031e425db0f26
Guilherme <gb9756092@gmail.com>
2025-12-09 20:22:37 -0300

    Adicionando a Função carregar dados do json

--- logica_crud.py
+++ logica_crud.py
@@ -10,30 +10,3 @@ def carregar_dados():
         with open (arquivo, "r", encoding="utf-8") as f:
             return json.load(f)
 
-def salvar_dados(perguntas):
-    with open(arquivo, "w", encoding="utf-8") as f:
-        json.dump(perguntas, f, indent=4, ensure_ascii=False)
-
-# Função CRUD        
-def adicionar(perguntas, nova):
-    try:
-      perguntas.append(nova)
-      salvar_dados(perguntas)
-    except ValueError:
-        messagebox.showerror("É necessario preencher o campo vazio.")
-
-def atualizar(perguntas, indice, nova):
-    try:
-     perguntas[indice] = nova
-     salvar_dados(perguntas)
-    except ValueError:
-        messagebox.showerror("É necessario preencher o campo vazio.")
-
-def excluir(perguntas, indice):
-    perguntas.pop(indice)
-    salvar_dados(perguntas)                             
-
-# Função de Limpar
-def limpar(perguntas):
- perguntas.delete(0, tk.END) 
-
